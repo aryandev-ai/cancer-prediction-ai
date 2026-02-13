e27a41222329d0c58b4c568c3f77fc2cc0ee55f2
aryandev-ai <saha42031@gmail.com>
2026-02-13 16:58:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+models = {
+    "Breast": joblib.load("breast.joblib"),
+    "Lung": joblib.load("lung.joblib"),
+    "Oral": joblib.load("oral.joblib"),
+    "Thyroid": joblib.load("thyroid.joblib")
+}
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    cancer = request.form["type"]
+    model = models[cancer]
+
+    values = [
+        float(request.form["f1"]),
+        float(request.form["f2"]),
+        float(request.form["f3"]),
+        float(request.form["f4"]),
+        float(request.form["f5"])
+    ]
+
+    import numpy as np
+
+    needed = model.n_features_in_
+
+    while len(values) < needed:
+        values.append(0)
+
+    sample = np.array(values).reshape(1, -1)
+
+    prediction = model.predict(sample)[0]
+    prob = model.predict_proba(sample)[0][1]
+
+    result = "High Risk" if prediction == 1 else "Low Risk"
+
+    confidence = round(prob * 100, 2)
+
+    return render_template(
+        "result.html",
+        result=result,
+        cancer=cancer,
+        confidence=confidence
+    )
+
+app.run(debug=True)
+
+
---
+++ train_models.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import joblib
+import matplotlib.pyplot as plt
+
+cancers = ["breast", "lung", "oral", "thyroid"]
+accuracies = []
+
+for c in cancers:
+    print("Training:", c)
+
+    data = pd.read_csv(f"datasets/{c}.csv")
+    data = data.apply(lambda col: col.astype('category').cat.codes)
+
+    X = data.iloc[:, :-1]
+    y = data.iloc[:, -1]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    accuracies.append(acc)
+
+    joblib.dump(model, f"{c}.joblib")
+
+# plot accuracy graph
+plt.bar(cancers, accuracies)
+plt.title("Model Accuracy")
+plt.ylabel("Accuracy")
+plt.show()
